a7541a3f626e3d12024f761a4f350e69ba313f75
maicka <maicka1997@gmail.com>
2022-07-05 03:42:21 +0200

    Agregar archivos de proyecto.

---
+++ ejercicio4.0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio4
+{
+    internal class Class1
+    {
+        static void Main(string[] args)
+        {
+            int N = 20;
+
+            int A = N + 77;
+            int B = A - 3;
+            int C = B * 2;
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(C);
+
+
+        }
+
+    }
+}
